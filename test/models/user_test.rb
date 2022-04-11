4f6218c5f5d4fda00a68ef33cbab0879c7c50c8b
Haussmann000 <duf000simon@gmail.com>
2022-04-11 22:29:45 +0900

    メールアドレスのバリデーションを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,8 +8,24 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+
   test "name should be present" do
     @user.name = "    "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.name = "    "
+    assert_not @user.valid?
+  end
+
+  test "name should note be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should note be too long" do
+    @user.name = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
 
